a6e89c91c81e792b718b4cb8033b68523ed9f001
vekintseva <vikivek@gmail.com>
2021-11-28 16:11:08 -0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/sort1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort1.dir/main.cpp.o"
+  "sort1"
+  "sort1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sort1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sort1.
+# This may be replaced when dependencies are built.
